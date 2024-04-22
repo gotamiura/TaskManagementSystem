e6432f81a29601461644e3b4ce09d7cfd7fe7737
gotamiura <gotamiu555@gmail.com>
2024-04-22 16:39:58 +0900

    EnterCommentsBean

---
+++ src/main/java/model/entity/EnterCommentsBean.java
@@ -0,0 +1,85 @@
+package model.entity;
+
+public class EnterCommentsBean {
+	private int commentId;
+	private int taskId;
+	private String taskName;
+	private String userId;
+	private String userName;
+	private String comment;
+	/**
+	 * @return commentId
+	 */
+	public int getCommentId() {
+		return commentId;
+	}
+	/**
+	 * @param commentId セットする commentId
+	 */
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	/**
+	 * @return taskId
+	 */
+	public int getTaskId() {
+		return taskId;
+	}
+	/**
+	 * @param taskId セットする taskId
+	 */
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+	/**
+	 * @return taskName
+	 */
+	public String getTaskName() {
+		return taskName;
+	}
+	/**
+	 * @param taskName セットする taskName
+	 */
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	/**
+	 * @return userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId セットする userId
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName セットする userName
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return comment
+	 */
+	public String getComment() {
+		return comment;
+	}
+	/**
+	 * @param comment セットする comment
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	
+}
