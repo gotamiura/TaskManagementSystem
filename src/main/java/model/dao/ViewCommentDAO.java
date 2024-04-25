8d8ddcad5e7bd5824a613dab22bfada63ed78d1f
shanmathi220 <shanmathi220@gmail.com>
2024-04-25 13:46:24 +0900

    コメント閲覧

--- src/main/java/model/dao/ViewCommentDAO.java
+++ src/main/java/model/dao/ViewCommentDAO.java
@@ -11,28 +11,26 @@ import java.util.List;
 import model.entity.EnterCommentsBean;
 
 public class ViewCommentDAO {
-	
+
 	public EnterCommentsBean selectComments(int taskId) throws SQLException, ClassNotFoundException {
 		EnterCommentsBean enterComments = null;
 
-		try(
-		Connection con = ConnectionManager.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(
-				"SELECT t1.task_id, t1.task_name, t3.user_id, t3.user_name, t2.comment FROM task_db.t_task t1 JOIN task_db.t_comment t2 ON t1.task_id = t2.task_id JOIN task_db.m_user t3 ON t1.user_id = t3.user_id WHERE t1.task_id = ?"))
-		{
-	        pstmt.setInt(1, taskId);
-	        try (ResultSet res = pstmt.executeQuery()) {
-	            if (res.next()) {
-	            	enterComments = new EnterCommentsBean();
-	            	enterComments.setTaskId(res.getInt("task_id"));
-	            	enterComments.setTaskName(res.getString("task_Name"));
-	                enterComments.setUserId(res.getString("user_id"));
-	                enterComments.setUserName(res.getString("user_name"));
-	            	enterComments.setComment(res.getString("comment"));
-	            }
-	        }
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(
+						"SELECT t1.task_id, t1.task_name, t3.user_id, t3.user_name, t2.comment FROM task_db.t_task t1 JOIN task_db.t_comment t2 ON t1.task_id = t2.task_id JOIN task_db.m_user t3 ON t1.user_id = t3.user_id WHERE t1.task_id = ?")) {
+			pstmt.setInt(1, taskId);
+			ResultSet res = pstmt.executeQuery();
+			if (res.next()) {
+				enterComments = new EnterCommentsBean();
+				enterComments.setTaskId(res.getInt("task_id"));
+				enterComments.setTaskName(res.getString("task_Name"));
+				enterComments.setUserId(res.getString("user_id"));
+				enterComments.setUserName(res.getString("user_name"));
+				enterComments.setComment(res.getString("comment"));
+			}
+
 		}
-		
+
 		return enterComments;
 	}
 
@@ -45,7 +43,7 @@ public class ViewCommentDAO {
 
 			pstmt.setInt(1, taskId);
 			//pstmt.setString(2, userId);
-			
+
 			ResultSet res = pstmt.executeQuery();
 			while (res.next()) {
 				int commentId = res.getInt("comment_id");
@@ -55,7 +53,7 @@ public class ViewCommentDAO {
 				String userName = res.getString("user_name");
 				String comment = res.getString("comment");
 				Timestamp updateDate = res.getTimestamp("update_datetime");
-						
+
 				EnterCommentsBean commentBean = new EnterCommentsBean();
 				commentBean.setCommentId(commentId);
 				commentBean.setTaskId(taskId1);
@@ -64,7 +62,7 @@ public class ViewCommentDAO {
 				commentBean.setUserName(userName);
 				commentBean.setComment(comment);
 				commentBean.setUpdateDatetime(updateDate);
-				
+
 				commentList.add(commentBean);
 			}
 		}
